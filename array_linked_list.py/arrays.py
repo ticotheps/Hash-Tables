4173dcaddae8b139c9d96d419d707df1f8864a35
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-08 13:19:44 -0400

    Defines 3 methods in the 'array' class

--- array_linked_list.py/arrays.py
+++ array_linked_list.py/arrays.py
@@ -25,26 +25,32 @@ def resize_array(array):
 def array_read(array, index):
     # Throw an error if array is out of the current count
     if index >= array.count:
-        print("Error out of bounds")
+        print("Error out of bounds in array_read")
         return None
     
     return array.elements[index]
 
 
 # Insert an element in a given array at a given index
-def array_insert(array, index):
+def array_insert(array, element, index):
     # Throw an error if array is out of the current count
-    if index >= array.count:
-        print("Error out of bounds")
+    if index > array.count:
+        print("Error out of bounds in array_insert")
         return None
     # Resize the array if the number of elements is over capacity
-
+    if array.capacity <= array.count:
+        resize_array(array)
+        
     # Move the elements to create a space at 'index'
-    # Think about where to start!
+    #   -Start at array.count (the last element in the array)
+    #   -Move backwards from array.count
+    #   -Stop when you get to the index
+    for i in range(array.count, index, -1):
+        array.elements[i] = array.elements[i - 1]
 
     # Add the new element to the array and update the count
-    pass
-
+    array.elements[index] = element
+    array.count += 1
 
 # Add an element to the end of the given array
 def array_append():
@@ -85,14 +91,14 @@ def array_print(array):
 
 # # Testing
 
-# arr = array(1)
+arr = array(1)
 
-# array_insert(arr, "STRING1", 0)
+array_insert(arr, "STRING1", 0)
 # array_print(arr)
 # array_pop(arr, 0)
 # array_print(arr)
-# array_insert(arr, "STRING1", 0)
+array_insert(arr, "STRING1", 0)
 # array_append(arr, "STRING4")
 # array_insert(arr, "STRING2", 1)
 # array_insert(arr, "STRING3", 2)
-# array_print(arr)
+array_print(arr)
