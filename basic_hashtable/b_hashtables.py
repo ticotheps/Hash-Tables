78a0d6bb233a6433d8461ae41142addaa8bc4d95
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-09 13:28:27 -0400

    Defines the 'hash_table_receive()' method

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -16,7 +16,7 @@ class Pair:
 class BasicHashTable:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.count = 0
+        # self.count = 0
         self.storage = [None] * self.capacity
         
 # '''
@@ -60,46 +60,43 @@ def hash_table_insert(hash_table, key, value):
 
 # If you try to remove a value that isn't there, print a warning.
 # '''
-# def hash_table_remove(hash_table, key):
-#     # Uses the 'hash()' function to get the index value in order to 
-#     #   access & delete the value at the passed-in 'key' parameter
-#     index = hash(key, hash_table.capacity)
-#     # If the cell at that 'index' (the index value) is empty, which
-#     #   means that the desired key/value pair doesn't exist... 
-#     if hash_table.storage[index] is None:
-#         # ...return 'False' because there's nothing to delete!
-#         return False
-#     # ...if the cell at that 'index' is NOT empty, then iterate through
-#     #    the pairs at that cell (using 'range' which gives you access to the
-#     #    index of an item for deletion)...
-#     for i in range(0, len(hash_table[index])):
-#         # ...if the passed-in 'key' parameter DOES exist within the items
-#         #   at this cell...
-#         if hash_table[index][i][0] == key:
-#             hash_table[index].pop(i)
-#             return True
+def hash_table_remove(hash_table, key):
+    # Uses the 'hash()' function to get the index value in order to 
+    #   access & delete the value at the passed-in 'key' parameter
+    index = hash(key, hash_table.capacity)
+    # If the cell at that 'index' (the index value) is empty, which
+    #   means that the desired key/value pair doesn't exist... 
+    if hash_table.storage[index] is None:
+        # ...return 'False' because there's nothing to delete!
+        return False
+    # ...if the cell at that 'index' is NOT empty, then iterate through
+    #    the pairs at that cell (using 'range' which gives you access to the
+    #    index of an item for deletion)...
+    for i in range(0, len(hash_table[index])):
+        # ...if the passed-in 'key' parameter DOES exist within the items
+        #   at this cell...
+        if hash_table[index][i][0] == key:
+            hash_table[index].pop(i)
+            return True
 
 # '''
 # Fill this in.
 
 # Should return None if the key is not found.
 # '''
-# def hash_table_retrieve(hash_table, key):
-#     # Uses the 'hash()' function to get the index value in order to 
-#     #   access & retrieve a value for the passed-in 'key' parameter
-#     index = hash(key, hash_table.capacity)
-#     # If the cell at that 'index' (the index value) is empty... 
-#     if hash_table.storage[index] is None:
-#         # Iterate through each pair in that cell to check if the passed-in
-#         #   'key' parameter is equal to any of them...
-#         for pair in hash_table.storage[index]:
-#             # ...if the passed-in 'key' parameter DOES already exists...
-#             if pair[0] == key:
-#                 # ...return that value
-#                 return pair[1]
-#     # If the passed-in 'key' parameter does not exist in the cell at that
-#     #   index, return 'None'
-#     return None
+def hash_table_retrieve(hash_table, key):
+    # Uses the 'hash()' function to get the index in order to 
+    #   access & retrieve a value for the passed-in 'key' parameter
+    index = hash(key, hash_table.capacity)
+    # If the bucket at 'index' is not empty... 
+    if hash_table.storage[index] is not None:
+        # ...and if the key in the bucket is the same as the passed-in key...
+        if hash_table.storage[index].key == key:
+            return hash_table.storage[index].value
+      
+    print("Unable to find value with key " + key)
+    return None
+
 
 def Testing():
     print(hash("hello world", 10))
@@ -111,10 +108,10 @@ def Testing():
 
     # hash_table_remove(ht, "line")
 
-    # if hash_table_retrieve(ht, "line") is None:
-    #     print("...gone tomorrow (success!)")
-    # else:
-    #     print("ERROR:  STILL HERE")
+    if hash_table_retrieve(ht, "line") is None:
+        print("...gone tomorrow (success!)")
+    else:
+        print("ERROR:  STILL HERE")
 
 
 Testing()
