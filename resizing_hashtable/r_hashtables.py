a9581e22cf5507069bcd3d9e7e5b298e9c3b7b4d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-10 20:49:47 -0400

    Updates 'r_hashtables.py' with the solution code

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -6,20 +6,15 @@ class LinkedPair:
         self.key = key
         self.value = value
         self.next = None
-    def __str__(self):
-        return f'key: {self.key}, value: {self.value}'
 
 
 # '''
-# Fill this in
-
 # Resizing hash table
 # '''
 class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.count = 0
-        self.storage = [None] * capacity 
+        self.storage = [None] * capacity
 
 
 # '''
@@ -27,106 +22,111 @@ class HashTable:
 # '''
 def hash(string, max):
     hash = 5381
-    for x in string:
-        hash = (( hash << 5) + hash) + ord(x)
+    for char in string:
+        hash = ((hash << 5) + hash) + ord(char)
+
     return hash % max
 
 
 # '''
 # Fill this in.
 
-# Hint: Used the LL to handle collisions
+# Hint: Used the LL handle collisions
 # '''
-def recursion1(node):
-    while node.next != None:
-        node = node.next
-        recursion1(node)
-
-
-
 def hash_table_insert(hash_table, key, value):
-    index = hash(key, hash_table.capacity)
-    insertThis = LinkedPair(key, value)
-    bucketRootNode = hash_table.storage[index]
+    index = hash(key, len(hash_table.storage))
 
-    if bucketRootNode is None:
-        print("if: ", hash_table.storage[index])
-        hash_table.storage[index] = insertThis
-        print("if (after): ", hash_table.storage[index])
-        print(hash_table.storage[index])
-        return True
+    current_pair = hash_table.storage[index]
+    last_pair = None
+
+    while current_pair is not None and current_pair.key != key:
+        last_pair = current_pair
+        current_pair = last_pair.next
+
+    if current_pair is not None:
+        current_pair.value = value
     else:
-        print("else: ", bucketRootNode)
-        recursion1(bucketRootNode)
-
-    bucketRootNode.next = insertThis
-    hash_table.count += 1
-
-HT = HashTable(10)
-hash_table_insert(HT, 'John', "Smith")
-print(HT.storage)
-print(HT.storage[0])
-        
-
-def recursion2(node, key):
-    counter = 0
-    prev_node = None
-    current_node = None
-    while node.key != key or node.next is not None:
-        prev_node = node
-        node = node.next
-        current_node = node
-        counter += 1
-        recursion2(node, key)
-    return prev_node
+        new_pair = LinkedPair(key, value)
+        new_pair.next = hash_table.storage[index]
+        hash_table.storage[index] = new_pair
 
 
+# '''
+# Fill this in.
+
+# If you try to remove a value that isn't there, print a warning.
+# '''
 def hash_table_remove(hash_table, key):
-    index = hash(key, hash_table.capacity)
-    bucketRootNode = hash_table.storage[index]
+    index = hash(key, len(hash_table.storage))
 
-    if hash_table.storage[index] is None:
-        return None
+    current_pair = hash_table.storage[index]
+    last_pair = None
+
+    while current_pair is not None and current_pair.key != key:
+        last_pair = current_pair
+        current_pair = last_pair.next
+
+    if current_pair is None:
+        print("ERROR: Unable to remove entry with key " + key)
     else:
-        prev_node = recursion2(bucketRootNode, key)[0]
-        current_node = recursion2(bucketRootNode, key)[1]
-
-    # '''
-    # Fill this in.
-
-    # Should return None if the key is not found.
-    # '''
+        if last_pair is None:  # Removing the first element in the LL
+            hash_table.storage[index] = current_pair.next
+        else:
+            last_pair.next = current_pair.next
 
 
+# '''
+# Fill this in.
+
+# Should return None if the key is not found.
+# '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    index = hash(key, len(hash_table.storage))
+
+    current_pair = hash_table.storage[index]
+
+    while current_pair is not None:
+        if(current_pair.key == key):
+            return current_pair.value
+        current_pair = current_pair.next
 
 
 # '''
 # Fill this in
 # '''
 def hash_table_resize(hash_table):
-    pass
+    new_hash_table = HashTable(2 * len(hash_table.storage))
+
+    current_pair = None
+
+    for i in range(len(hash_table.storage)):
+        current_pair = hash_table.storage[i]
+        while current_pair is not None:
+            hash_table_insert(new_hash_table,
+                              current_pair.key,
+                              current_pair.value)
+            current_pair = current_pair.next
+
+    return new_hash_table
 
 
-# def Testing():
-#     ht = HashTable(2)
+def Testing():
+    ht = HashTable(2)
 
-#     hash_table_insert(ht, "line_1", "Tiny hash table")
-#     hash_table_insert(ht, "line_2", "Filled beyond capacity")
-#     hash_table_insert(ht, "line_3", "Linked list saves the day!")
+    hash_table_insert(ht, "line_1", "Tiny hash table")
+    hash_table_insert(ht, "line_2", "Filled beyond capacity")
+    hash_table_insert(ht, "line_3", "Linked list saves the day!")
 
-#     print(hash_table_retrieve(ht, "line_1"))
-#     print(hash_table_retrieve(ht, "line_2"))
-#     print(hash_table_retrieve(ht, "line_3"))
+    print(hash_table_retrieve(ht, "line_1"))
+    print(hash_table_retrieve(ht, "line_2"))
+    print(hash_table_retrieve(ht, "line_3"))
 
-#     old_capacity = len(ht.storage)
-#     ht = hash_table_resize(ht)
-#     new_capacity = len(ht.storage)
+    old_capacity = len(ht.storage)
+    ht = hash_table_resize(ht)
+    new_capacity = len(ht.storage)
 
-#     print("Resized hash table from " + str(old_capacity)
-#           + " to " + str(new_capacity) + ".")
+    print("Resized hash table from " + str(old_capacity)
+          + " to " + str(new_capacity) + ".")
 
 
-# Testing()
- 
+Testing()
